1c9de1c4bc97f0293a3df67c042916f17309906d
Orest-Maiatskyi <orestkiev@gmail.com>
2025-01-10 11:39:04 +0200

    Added db-model creation at first application startup, added dao error handler

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,7 +3,23 @@ from flask import Flask
 from app.config import current_config
 from app.ext import *
 
-api = Flask(__name__)
-api.config.from_object(current_config)
+app = Flask(__name__)
+app.config.from_object(current_config)
 
-init_ext(api)
+init_ext(app)
+
+
+from app.database import *
+
+
+with app.app_context():
+    if app.config.get('DEBUG'):
+        db.drop_all()
+        db.create_all()
+        app.logger.info('The application was launched in DEBUG mode, all DB columns were dropped!')
+    db.create_all()
+
+
+@app.errorhandler(DAOException)
+def handle_dao_error(error):
+    return {'error': 'Internal Server Error', 'message': 'Something went wrong :('}, 500
