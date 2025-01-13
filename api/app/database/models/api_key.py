3ebeea7478bf865a62442e2430d86ed008efeefd
Orest Maiatskyi <orestkiev@gmail.com>
2025-01-13 17:43:31 +0200

    Added api_key.py - ApiKeyModel

---
+++ api/app/database/models/api_key.py
@@ -0,0 +1,25 @@
+import uuid
+from datetime import datetime, timezone
+
+from sqlalchemy import Integer, String, Boolean, DateTime, ForeignKey, JSON
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app import db
+
+
+class ApiKey(db.Model):
+    __tablename__ = 'api_keys'
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    uuid: Mapped[str] = mapped_column(String(36), unique=True, nullable=False, default=lambda: str(uuid.uuid4()))
+    key: Mapped[str] = mapped_column(String(32), unique=True, nullable=False,
+                                     default=lambda: str(uuid.uuid4()).replace('-', ''))
+    name: Mapped[str | None] = mapped_column(String(100), nullable=True)
+    domains: Mapped[dict] = mapped_column(JSON, nullable=True)
+    registered_at: Mapped[datetime] = mapped_column(DateTime, default=datetime.now(timezone.utc), nullable=False)
+    is_deleted: Mapped[bool] = mapped_column(Boolean(), default=False, nullable=False)
+    deleted_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
+
+    user_uuid: Mapped[str] = mapped_column(String(36), ForeignKey('users.uuid'), nullable=False)
+    user = relationship('User', back_populates='api_keys')
+
+    fine_tuning = relationship('FineTuning', back_populates='api_key', cascade='all, delete-orphan')
