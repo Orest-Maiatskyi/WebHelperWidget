78577cd4520489b1b1d381a0052982d42a637e5e
Orest Maiatskyi <orestkiev@gmail.com>
2025-01-13 17:40:38 +0200

    Added imports to __init__.py

--- api/app/database/dao/__init__.py
+++ api/app/database/dao/__init__.py
@@ -1,2 +1,3 @@
 from .base import Base as BaseDAO
 from .user import User as UserDAO
+from .fine_tuning import FineTuning as FineTuningDAO
--- api/app/database/models/__init__.py
+++ api/app/database/models/__init__.py
@@ -1,1 +1,3 @@
 from .user import User as UserModel
+from .api_key import ApiKey as ApiKeyModel
+from .fine_tuning import FineTuning as FineTuningModel
