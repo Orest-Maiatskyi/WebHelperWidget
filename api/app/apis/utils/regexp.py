870a9c3abb13f050d2fea3564e03f0ba6ce75c89
Orest-Maiatskyi <orestkiev@gmail.com>
2025-01-14 22:38:44 +0200

    Updated regexp.py, added new regexp for api key route

--- api/app/apis/utils/regexp.py
+++ api/app/apis/utils/regexp.py
@@ -1,5 +1,5 @@
 
-email_regexp = '^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+email_regexp = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
 """
 This regular expression validates email addresses.
 
@@ -39,24 +39,41 @@ This regex validates a password with specific complexity requirements.
 - `Ab1@` (less than 8 characters)
 """
 
-name_regexp = '^[a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ]{3,50}$'
+name_regexp = r'^[a-zA-Zа-яА-ЯёЁіІїЇєЄґҐ]{3,50}$'
 """
 This regular expression matches a string that consists of between 3 and 50 characters, which can be English or 
 Cyrillic letters (including Russian and Ukrainian), and the Russian "ё" in both cases. The string cannot contain 
 spaces, punctuation, or other symbols.
 """
 
-math_captcha_answer_regexp = '^(?:[1-9][0-9]{0,2}|1000)$'
+math_captcha_answer_regexp = r'^(?:[1-9][0-9]{0,2}|1000)$'
 """
 This regular expression for matching numbers that are greater than 0 but less than or equal to 1000:
 """
 
-removal_reason_regexp = '^.{10,255}$'
+removal_reason_regexp = r'^.{10,255}$'
 """
 This regular expression for a string to be at least 10 characters and at most 255 characters.
 """
 
-uuid4_regexp = '^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$'
+uuid4_regexp = r'^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$'
 """
 This regular expression for a uuid4 string verification.
+"""
+
+api_key_name_regexp = r'^.{1, 100}$'
+"""
+This regular expression for api key optional description, min 1 symbol, max 100.
+"""
+
+api_key_domains_regexp = r'^(?:[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(?:,\s*[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})*$'
+"""
+This regular expression that matches a string containing domains separated by commas.
+
+### Examples:
+    example.com,example.org 	       ✅ Yes
+    example.com, example.org,sub.net   ✅ Yes
+    example.com,"	                   ❌ No
+    example,com,example.org            ❌ No
+    example.com,,example.org           ❌ No
 """
