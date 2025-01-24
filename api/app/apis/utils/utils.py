f8dc0ca0953690ed0236c1c334e07d9da82e63a6
Orest-Maiatskyi <orestkiev@gmail.com>
2025-01-24 18:14:51 +0200

    Updated utils.py, added authenticator decorator, this decorator combines the jwt_required functionality with account validation

--- api/app/apis/utils/utils.py
+++ api/app/apis/utils/utils.py
@@ -1,5 +1,6 @@
 import random
 import time
+from datetime import datetime, timezone
 
 import regex as re
 from functools import wraps
@@ -7,9 +8,10 @@ from typing import Callable, Dict
 
 from flask import request, jsonify
 from flask_jwt_extended import get_jwt
+from flask_jwt_extended.view_decorators import LocationType, verify_jwt_in_request
 from werkzeug.exceptions import MethodNotAllowed
 
-from app import redis_client, app
+from app import redis_client, app, UserDAO
 from app.apis.utils import math_captcha_answer_regexp
 
 
@@ -127,6 +129,97 @@ def arg_parser(required_args: Dict[str, str] | None = None,
                 validated_args[f'{file_required}_file'] = file
 
             return func(*args, **kwargs, **validated_args)
+
+        return wrapper
+
+    return decorator
+
+
+def authenticator(
+        optional: bool = False,
+        fresh: bool = False,
+        refresh: bool = False,
+        locations: LocationType | None = None,
+        verify_type: bool = True,
+        skip_revocation_check: bool = False) -> Callable:
+    """
+    A decorator to protect a Flask endpoint with JSON Web Tokens + validating user account
+
+    :param optional:
+        If ``True``, allow the decorated endpoint to be accessed if no JWT is present in
+        the request. Defaults to ``False``.
+
+    :param fresh:
+        If ``True``, require a JWT marked with ``fresh`` to be able to access this
+        endpoint. Defaults to ``False``.
+
+    :param refresh:
+        If ``True``, requires a refresh JWT to access this endpoint. If ``False``,
+        requires an access JWT to access this endpoint. Defaults to ``False``.
+
+    :param locations:
+        A location or list of locations to look for the JWT in this request, for
+        example ``'headers'`` or ``['headers', 'cookies']``. Defaults to ``None``
+        which indicates that JWTs will be looked for in the locations defined by the
+        ``JWT_TOKEN_LOCATION`` configuration option.
+
+    :param verify_type:
+        If ``True``, the token type (access or refresh) will be checked according
+        to the ``refresh`` argument. If ``False``, type will not be checked and both
+        access and refresh tokens will be accepted.
+
+    :param skip_revocation_check:
+        If ``True``, revocation status of the token will be *not* checked. If ``False``,
+        revocation status of the token will be checked.
+    """
+    def decorator(func: Callable):
+        """
+        A decorator for validating user account status based on JWT claims and user data from the database.
+
+        This checks:
+        - If the JWT is valid (returning a 401 if app was rebooted and jwt is still valid).
+        - If the account email is verified, returning a 403 Forbidden response.
+        - If the account is deleted, returning a 410 Gone response.
+        - If the account is blocked, returning a 423 Locked response with details.
+        - If the block has expired, it updates the user's block status in the database.
+
+        :param func: The function to decorate.
+        :type func: Callable
+        :return: The wrapped function.
+        :rtype: Callable
+        """
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            verify_jwt_in_request(optional, fresh, refresh, locations, verify_type, skip_revocation_check)
+
+            if not optional:
+                user = UserDAO.get_user_by_uuid(get_jwt().get("sub"))
+
+                if user is None:
+                    return jsonify({'error': 'Unauthorized', 'message': 'Invalid or expired JWT token'}), 401
+
+                if not user.email_verified:
+                    return jsonify({'error': 'Forbidden', 'message': 'Email address not verified'}), 403
+
+                if user.is_deleted:
+                    return jsonify({'error': 'Gone', 'message': 'Account was deleted'}), 410
+
+                if user.is_blocked:
+                    time_diff = datetime.fromisoformat(user.blocked_until) - datetime.now(timezone.utc)
+
+                    if time_diff.total_seconds() > 0:
+                        return jsonify({
+                            'error': 'Locked',
+                            'message': 'Account blocked',
+                            'blocked_until': user.blocked_until,
+                            'block_reason': user.blocked_reason
+                        }), 423
+                    else:
+                        user.is_blocked = False
+                        user.blocked_reason = None
+                        UserDAO.commit()
+
+            return func(*args, **kwargs)
 
         return wrapper
 
