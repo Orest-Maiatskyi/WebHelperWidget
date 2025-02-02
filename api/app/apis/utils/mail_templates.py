ea7cc44a484bacbed1e468f55a96fcfb72f2dab2
Orest-Maiatskyi <orestkiev@gmail.com>
2025-02-02 15:22:50 +0200

    Added mail_templates.py, file contains email html templates

---
+++ api/app/apis/utils/mail_templates.py
@@ -0,0 +1,24 @@
+
+
+confirm_email_template = \
+    """
+    <html>
+      <body>
+        <h2 style="color: #4CAF50;">Приветствуем, {{ name }}!</h2>
+        <p>Спасибо за регистрацию, для подтверждения почты пкрейдите по ссылке ниже:</p>
+        <a href="{{ confirm_link }}" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none;">Подтвердить Email</a>
+      </body>
+    </html>
+    """
+
+
+forgot_password_template = \
+    """
+    
+    """
+
+
+password_changed_template = \
+    """
+    
+    """
