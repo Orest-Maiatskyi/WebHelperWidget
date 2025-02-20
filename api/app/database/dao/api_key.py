7d92fd8372840a98566648c86b7241b9b8e87f61
Orest-Maiatskyi <orestkiev@gmail.com>
2025-02-20 13:11:05 +0200

    Updated api_key.py, fixed bug: an attempt to create an api key resulted in a DAOException due to an attempt to call a transaction inside an existing transaction

--- api/app/database/dao/api_key.py
+++ api/app/database/dao/api_key.py
@@ -11,19 +11,18 @@ class ApiKey(Base):
     def create_api_key(user_uuid: str, key: str | None = None, name: str | None = None,
                        domains: dict | None = None, is_deleted: bool | None = None,
                        deleted_at: datetime | None = None) -> None:
-        def transaction():
-            api_key = ApiKeyModel(
-                key=key,
-                name=name,
-                domains=domains,
-                is_deleted=is_deleted,
-                deleted_at=deleted_at,
-                user_uuid=user_uuid)
-            db.session.add(api_key)
-            db.session.flush()
-            fine_tuning = FineTuningModel(api_key_uuid=api_key.uuid)
-            db.session.add(fine_tuning)
-        ApiKey.transaction(transaction)
+        api_key = ApiKeyModel(
+            key=key,
+            name=name,
+            domains=domains,
+            is_deleted=is_deleted,
+            deleted_at=deleted_at,
+            user_uuid=user_uuid)
+        db.session.add(api_key)
+        db.session.flush()
+        fine_tuning = FineTuningModel(api_key_uuid=api_key.uuid)
+        db.session.add(fine_tuning)
+        db.session.commit()
 
     @staticmethod
     def get_api_key_by_id(_id: int | str) -> ApiKeyModel | None:
@@ -35,4 +34,4 @@ class ApiKey(Base):
 
     @staticmethod
     def get_all_api_keys_by_user_uuid(user_uuid: str) -> list[ApiKeyModel]:
-        return ApiKey.scalars_query(ApiKeyModel.query.where(ApiKeyModel.user_uuid == user_uuid).all())
+        return ApiKey.scalars_query(ApiKeyModel.query.where(ApiKeyModel.user_uuid == user_uuid))
