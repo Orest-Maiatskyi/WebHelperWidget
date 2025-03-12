3420357f64fd1ca13143d4f82efce15618f2caf8
Orest-Maiatskyi <orestkiev@gmail.com>
2025-03-12 22:52:01 +0200

    Updated __init__.py, new imports added

--- api/app/apis/__init__.py
+++ api/app/apis/__init__.py
@@ -1,4 +1,5 @@
-from .auth import auth_bp
+from app.apis.header_auth import header_auth_bp
+from app.apis.cookie_auth import cookie_auth_bp
 from .confirm_email import confirm_email_bp
 from .account import account_bp
 from .api_key import api_key_bp
